b59a4aa4c9fd695a7c09f6b7caba172cf57af4e5
Denise Sonia Rahmadina <89030834+denisesonia@users.noreply.github.com>
2023-02-19 13:15:10 -0500

    map code

--- app/ui.R
+++ app/ui.R
@@ -1,74 +1,65 @@
+library(shiny)
+library(leaflet)
+library(shiny)
+library(shinydashboard)
+library(dygraphs)
 
-if (!require("shiny")) {
-  install.packages("shiny")
-  library(shiny)
-}
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
-if (!require("shinythemes")) {
-  install.packages("shinythemes")
-  library(shinythemes)
-}
-if (!require("leaflet")) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-if (!require("leaflet.extras")) {
-  install.packages("leaflet.extras")
-  library(leaflet.extras)
-}
+type <- c("Full Fare" = "Full Fare", "30-Day Unlimited"= "30-Day Unlimited",
+          "7-Day Unlimited" =  "7-Day Unlimited", "Student" = "Student",
+          "Senior Citizen/Disabled" = "Senior Citizen/Disabled", "Annual Metrocard" = "Annual Metrocard",
+          "EasyPayXpress" =  "EasyPayXpress", "Other Fares" = "Other Fares")
+header <- dashboardHeader(title = p("MTA Subway Ridership App"),
+                          titleWidth = 400)
 
-# Define UI for application that draws a histogram
-shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
+sidebar <- dashboardSidebar(
+  
+  sidebarUserPanel("ADS Spring 2023 Group 6"),
+  sidebarMenu(
+    
+    #menuItem("Hourly Collision Data", tabName = "data", icon = icon("bar-chart-o")),
+    #menuItem("Hourly Borough Comparison", tabName = "data_by_borough", icon = icon("bar-chart-o")),
+    #menuItem("Raw Fatality Map", tabName = "map", icon = icon("map")),
+    menuItem("Interactive MTA Subway Ridership Map", tabName = "leafletmap", icon = icon("map"))
+  )
+)
+
+body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
+  fluidRow(
+    column(width =4,
+           box(width = NULL, title =tagList(shiny::icon("filter",class = 'fa-lg'), "Filter Data") ,
+               solidHeader = T, collapsible = T, status = 'primary',
+               selectizeInput('fare_type','Fare Type', choices = type, width = 380,
+                              selected = c('Full Fare', '30-Day Unlimited',"7-Day Unlimited",
+                                           "Student","Senior Citizen/Disabled","Annual Metrocard",
+                                           "EasyPayXpress","Other Fares"),multiple = T),
+               dateRangeInput('dates', label = "Date Range",width = 380,
+                              start = '2019-11-01', end = '2022-01-01',
+                              min = "2019-11-01", max = "2022-01-01"
+               ),
+               submitButton(text = "Submit",icon =icon('filter'))
+           ),
+           box(width = NULL,title = tagList(shiny::icon("info-circle",class = 'fa-lg'), "About MTA Subway Dashboard"), solidHeader = T, collapsible = T, status = 'info',
+               strong("MTA Subway Dashboard"),"is an interactive map built on shiny which allows you to cutomize
+                         time range and fare type to filter out MTA Subway Ridership of a certain location you want",
+               "The data is from", a('here.', href = 'https://new.mta.info/open-data', target = "_blank")
+           ),
+           
+    ),
+    column(width =8,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput('subwayMap',height = 500)),
+           box(width = NULL, 
+               dygraphOutput('plot1')
+           )
+    )
+  )
+)
 
 
-    ) #navbarPage closing  
-) #Shiny UI closing    
+ui <- dashboardPage(skin = 'blue',
+                    header,
+                    dashboardSidebar(disable = T),
+                    body
+)
---
+++ doc/Mapping.R
@@ -0,0 +1,121 @@
+library(tidyverse) #For Data Manipulation and Plotting
+library(janitor) #For cleaning up the variable names in the CSV Files
+library(lubridate) #For date processing 
+library(patchwork) # For combining multiple ggplots together
+library(ggmap) # For producing a static map
+library(ggtext) # For adding some flair to ggplot
+library(leaflet) # For Making Interactive Plots
+library(rvest) # For Web Scraping Links to Download
+
+#Gather dataset from MTA Website
+dataset <- read_html("http://web.mta.info/developers/fare.html") %>%
+  html_nodes("a") %>% 
+  html_attr("href") %>% 
+  keep(str_detect(., 'fares_(19)|(22)\\d{4}\\.csv')) %>% 
+  map_dfr(., function(x){
+    return(
+      read_csv(paste0("http://web.mta.info/developers/", x), skip = 2) %>% 
+        clean_names %>%
+        #Drop Dead Columns
+        select_if(~!all(is.na(.x))) %>%
+        mutate(
+          key = str_extract(x, '\\d+'),
+          
+          #The data in the files covers seven-day periods beginning on the Saturday 
+          #two weeks prior to the posting date and ending on the following Friday. 
+          #Thus, as an example, the file labeled Saturday, January 15, 2011, has data 
+          #covering the period from Saturday, January 1, 2011, through Friday, January 7. 
+          #The file labeled January 22 has data covering the period from 
+          #Saturday, January 8, through Friday, January 14. And so on and so forth
+          week_start = ymd(paste0('20',key)) - days(14),
+          week_end = ymd(paste0('20',key)) - days(8)
+        ) %>%
+        mutate(across(c(-remote, -station, -week_start, -week_end, -key), parse_number)) %>% 
+        pivot_longer(
+          cols = c(-remote, -station, -week_start, -week_end, -key),
+          names_to = "fare_type",
+          values_to = "fares"
+        )
+    )
+  }
+  ) 
+
+#Distribution of ridership through out the years
+library(cowplot)
+dataset %>% 
+  group_by(key, week_start, week_end) %>% 
+  summarize(fares = sum(fares, na.rm = T), .groups = 'drop') %>% 
+  ggplot(aes(x = week_start, y = fares/1e6)) + 
+  geom_line(color = '#0039A6') + 
+  geom_vline(xintercept = ymd(20200322), lty = 2) + 
+  geom_point(data = tibble(
+    week_start = c(ymd(20200222), ymd(20200418)),
+    fares = c(30768135, 2548002)
+  ), color = 'red', size =3
+  ) +
+  geom_textbox(
+    x = ymd(20191001),
+    y = 15,
+    label = "A ***<span style = 'color:red'>92% Decrease</span>*** \n in Subway Ridership \n 1 month before \n vs. 1 month after \n PAUSE order",
+    fill = 'cornsilk',
+    halign = 0.5,
+  ) + 
+  labs(x = "Week Beginning", y = "# of MTA Subway Fares (millions)",
+       title = "<span style='color:#0039A6'>MTA</span> Ridership (Jan 2019 - Aug 2020)",
+       subtitle = "PAUSE Order Begins on 3/22/2020") + 
+  scale_y_continuous(labels = scales::comma) +
+  cowplot::theme_cowplot() + 
+  theme(
+    plot.title = element_markdown()
+  )
+
+#Data Cleaning on Ridership
+combined <- dataset %>% 
+  mutate(
+    fare_type = case_when(
+      fare_type == 'ff' ~ 'Full Fare',
+      fare_type == 'x30_d_unl' ~ '30-Day Unlimited',
+      fare_type == 'x7_d_unl' ~ '7-Day Unlimited',
+      fare_type == 'students' ~ 'Student',
+      fare_type == 'sen_dis' ~ 'Senior Citizen/Disabled',
+      fare_type == 'tcmc_annual_mc' ~ 'Annual Metrocard',
+      fare_type == 'mr_ezpay_exp' ~ 'EasyPayXpress',
+      TRUE ~ fare_type
+    )
+  ) %>% 
+  #Remove SBS Bus Stations and PATH
+  filter(!str_detect(station, "SBS-|PA-|AIRTRAIN"))
+
+top_7 <- combined %>% 
+  count(fare_type, sort = T) %>% 
+  head(7) %>% 
+  pull(fare_type)
+
+
+station_level <- combined %>% 
+  mutate(
+    fare_type = fct_other(fare_type, keep = top_7, other_level = "Other Fares")
+  ) %>% 
+  group_by(remote, station, fare_type) 
+
+geocodes <- read_csv('https://raw.githubusercontent.com/chriswhong/nycturnstiles/master/geocoded.csv', 
+                     col_names = c('remote', 'zuh', 'station', 'line', 'system', 'lat', 'long'),
+)
+comb_geo <- station_level %>% 
+  inner_join(geocodes %>% group_by(remote) %>% summarize(lat = max(lat), long = max(long)), by = "remote") %>%
+  ungroup()
+
+saveRDS(comb_geo, "comb_geo.RDS")
+write.csv(comb_geo, "mta_dataset.csv", row.names=FALSE)
+
+
+
+data <- as_tibble(fread('MTA_dataset.csv',header = T))
+
+
+
+
+
+
+
+
